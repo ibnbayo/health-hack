ce3a48f609f9a905a8c7e4f9b842776bbe5f47d0
ibnbayo <abdullahifahm@gmail.com>
2022-08-20 09:41:43 +0100

    pull data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import Header from './components/Header.js'
 import Footer from './components/Footer.js';
-
+import { useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+
+const options = {
+  method: 'GET',
+  url: 'https://edamam-recipe-search.p.rapidapi.com/search',
+  params: {q: 'rice'},
+  headers: {
+    'X-RapidAPI-Key': 'c0d2e70417msh3bde3b7dbe9e25ap12748ejsncd1fe394742c',
+    'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
+  }
+};
 function App() {
+
+  useEffect(()=>{
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }, [])
+
   return (
     <div className="App">
       <Header/>
       <Footer/>
+
       
     </div>
   );
